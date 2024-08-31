69bf60b89b62325c05dc1594952a3cbff4d05d9f
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-08-31 22:42:41 +0400

    Fixed bugs

--- src/js/controller.js
+++ src/js/controller.js
@@ -108,10 +108,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('WELCOME TO THE APPLICATION');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -120,6 +116,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResuluts);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
